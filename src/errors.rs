6573f9d541d2d5222514f672bb434027ccaa7a5c
Igor Artamonov <igor@artamonov.ru>
2025-03-21 21:49:23 +0000

    solution: impl std::error::Error fix: #3

--- src/errors.rs
+++ src/errors.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Display, Formatter};
+
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
 pub enum Error {
     HighBitIsSet,
@@ -6,3 +8,17 @@ pub enum Error {
     InvalidStructure,
     InvalidFormat
 }
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::HighBitIsSet => write!(f, "High bit is set"),
+            Error::InvalidLength(len) => write!(f, "Invalid length: {}", len),
+            Error::InvalidPurpose(purpose) => write!(f, "Invalid purpose: {}", purpose),
+            Error::InvalidStructure => write!(f, "Invalid structure"),
+            Error::InvalidFormat => write!(f, "Invalid format")
+        }
+    }
+}
+
+impl std::error::Error for Error {}
